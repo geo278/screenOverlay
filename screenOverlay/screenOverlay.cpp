099e2c9cd190105cd38debd15e17fff7a72d2335
George Zhang <43013270+geo278@users.noreply.github.com>
2019-12-21 17:10:40 -0800

    refactor to simplify

--- screenOverlay/screenOverlay.cpp
+++ screenOverlay/screenOverlay.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-float s = (float) 2;
+float s = (float) 5;
 float sr = (float)(1 - s) * 0.3086;
 float sg = (float)(1 - s) * 0.6094;
 float sb = (float)(1 - s) * 0.0820;
-MAGCOLOREFFECT g_MagEffectTransform = { sr + s,	sr,		sr,		0.0f,	0.0f,
+MAGCOLOREFFECT g_MagEffectSaturation = {sr + s,	sr,		sr,		0.0f,	0.0f,
 										sg,		sg + s,	sg,		0.0f,	0.0f,
 										sb,		sb,		sb + s,	0.0f,	0.0f,
                                         0.0f,	0.0f,	0.0f,	1.0f,	0.0f,
@@ -19,11 +19,6 @@ MAGCOLOREFFECT g_MagEffectIdentity = {1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
                                       0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
                                       0.0f,  0.0f,  0.0f,  1.0f,  0.0f,
                                       0.0f,  0.0f,  0.0f,  0.0f,  1.0f};
-
-BOOL transformColor(__in BOOL transformationOn) {// Apply the color matrix 
-    PMAGCOLOREFFECT pEffect = (transformationOn ? &g_MagEffectTransform : &g_MagEffectIdentity);
-    return MagSetFullscreenColorEffect(pEffect);
-}
 
 BOOL SetZoomB(float magFactor) {
 	if (magFactor < 1.0) {
@@ -40,12 +35,12 @@ int main() {
 		while (true) {
 			if ((GetKeyState(VK_RBUTTON) & 0x100) != 0) {
 				SetZoomB(2);
-				transformColor(TRUE);
+				MagSetFullscreenColorEffect(&g_MagEffectSaturation);
 				cout << "Enhance" << endl;
 				while ((GetKeyState(VK_RBUTTON) & 0x100) != 0) { Sleep(20); }
 			} else {
 				SetZoomB(1);
-				transformColor(FALSE);
+				MagSetFullscreenColorEffect(&g_MagEffectIdentity);
 				cout << "Restore" << endl;
 				while ((GetKeyState(VK_RBUTTON) & 0x100) == 0) { Sleep(2); }
 			}
