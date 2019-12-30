e68265934d31e94408e6da5a45c4694b18857ccc
George Zhang <43013270+geo278@users.noreply.github.com>
2019-12-30 15:16:34 -0800

    fix centering issues for virtual super resolution

--- screenOverlay/screenOverlay.cpp
+++ screenOverlay/screenOverlay.cpp
@@ -3,11 +3,10 @@
 #include "magnification.h"
 
 using namespace std;
-
 float s = (float) 1.1;
-float sr = (float)(1 - s) * 0.3086;
-float sg = (float)(1 - s) * 0.6094;
-float sb = (float)(1 - s) * 0.0820;
+float sr = (float)(1 - s) * (float)0.3086;
+float sg = (float)(1 - s) * (float)0.6094;
+float sb = (float)(1 - s) * (float)0.0820;
 MAGCOLOREFFECT g_MagEffectSaturation = {sr + s,	sr,		sr,		0.0f,	0.0f,
 										sg,		sg + s,	sg,		0.0f,	0.0f,
 										sb,		sb,		sb + s,	0.0f,	0.0f,
@@ -24,8 +23,15 @@ BOOL SetZoomB(float magFactor) {
 	if (magFactor < 1.0) {
 		return FALSE;
 	}
-	int xOffset = (int)((float)GetSystemMetrics(SM_CXSCREEN) * (1.0 - (1.0 / magFactor)) / 2.0);
-	int yOffset = (int)((float)GetSystemMetrics(SM_CYSCREEN) * (1.0 - (1.0 / magFactor)) / 2.0);
+	int xSize = GetSystemMetrics(SM_CXSCREEN);
+	int ySize = GetSystemMetrics(SM_CYSCREEN);
+	if (xSize == 2048 && ySize == 1152) {
+		xSize = 2560;
+		ySize = 1440;
+	}
+	int xOffset = (int)((float)xSize * (1.0 - (1.0 / magFactor)) / 2.0);
+	int yOffset = (int)((float)ySize * (1.0 - (1.0 / magFactor)) / 2.0);
+	//cout << (int)(float)GetSystemMetrics(SM_CXSCREEN) << "  " << (int)(float)GetSystemMetrics(SM_CYSCREEN) << endl;
 	return MagSetFullscreenTransform(magFactor, xOffset, yOffset);
 }
 
@@ -35,12 +41,12 @@ int main() {
 		while (true) {
 			if ((GetKeyState(VK_RBUTTON) & 0x100) != 0) {
 				SetZoomB(2);
-				MagSetFullscreenColorEffect(&g_MagEffectSaturation);
-				cout << "Enhance" << endl;
+				//MagSetFullscreenColorEffect(&g_MagEffectSaturation);
+				cout << "Zoom In" << endl;
 				while ((GetKeyState(VK_RBUTTON) & 0x100) != 0) { Sleep(20); }
 			} else {
 				SetZoomB(1);
-				MagSetFullscreenColorEffect(&g_MagEffectIdentity);
+				//MagSetFullscreenColorEffect(&g_MagEffectIdentity);
 				cout << "Restore" << endl;
 				while ((GetKeyState(VK_RBUTTON) & 0x100) == 0) { Sleep(2); }
 			}
